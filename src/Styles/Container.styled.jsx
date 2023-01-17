5c1e3735536dcc5a373d12274b0b3ad17d2aa0bd
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-17 16:12:57 +1300

    created, indexed and rendered container.styled
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/Styles/Container.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+	width: min(90%, var(--maxWidth));
+	margin: 0 auto;
+	display: grid;
+	place-items: center;
+
+`
+
+export default StyledContainer
