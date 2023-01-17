8cc2bf99a2ac7e62edbf69953c0d04c0629f2c82
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-18 12:05:22 +1300

    inndexed all the new compos
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Styles/index.jsx
+++ src/Styles/index.jsx
@@ -2,5 +2,14 @@ import GlobalStyles from './GlobalStyles'
 import StyledButton from './Button.styled'
 import StyledContainer from './Container.styled'
 import StyledHeader from './Header.styled'
+import StyledNav from './Nav.styled'
+import StyledLogo from './Logo.styled'
 
-export { GlobalStyles, StyledButton, StyledContainer, StyledHeader }
+export {
+	GlobalStyles,
+	StyledButton,
+	StyledContainer,
+	StyledHeader,
+	StyledNav,
+	StyledLogo,
+}
