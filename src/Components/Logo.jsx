165e4fb9254a124dfa5b2c409e175528afd87cfc
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-18 12:01:54 +1300

    created ,indexed and rendered the compo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import logo from '../Assets/logo.svg'
+import { StyledLogo } from '../Styles'
 
 const Logo = () => {
-  return (
-    <div>Logo goes here</div>
-  )
+	return <StyledLogo src={logo} alt='logo' />
 }
 
 export default Logo
---
+++ src/Styles/Logo.styled.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const StyledLogo = styled.img`
+	width: 3rem;
+	height: 3rem;
+  cursor: ;
+`
+export default StyledLogo
