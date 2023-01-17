999f9ccaca3813c0ca8227ace7521ed8cda21381
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-18 12:05:05 +1300

    set up the padding -inline for all the screens
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Styles/Container.styled.jsx
+++ src/Styles/Container.styled.jsx
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
 
 const StyledContainer = styled.div`
-	width: min(90vw, var(--maxWidth));
-	margin: 0 auto;
+	width: min(100vw, var(--maxWidth));
+	padding-inline: 1rem;
 	display: grid;
 	place-items: center;
-	@media (min-width: var(--desktop)) {
-		width: min(95vw, var(--maxWidth));
+	background-color: var(--lightCream);
+	@media (min-width: 768px) {
+		padding-inline: 2.5rem;
+	}
+	@media (min-width: 1440px) {
+		padding-inline: 10.25rem;
 	}
 `
 
