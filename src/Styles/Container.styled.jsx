24d4d1ab7a5bfdedf90a4cdb5da5dd86f39e28e4
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-19 09:51:26 +1300

    added dynamic screen padding for the container
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Styles/Container.styled.jsx
+++ src/Styles/Container.styled.jsx
@@ -2,15 +2,15 @@ import styled from 'styled-components'
 
 const StyledContainer = styled.div`
 	width: min(100vw, var(--maxWidth));
-	padding-inline: 1rem;
+	padding-inline: ${({fullWidth}) => fullWidth ? '0rem' : '1rem'};
 	display: grid;
 	place-items: center;
 	background-color: var(--lightCream);
 	@media (min-width: 768px) {
-		padding-inline: 2.5rem;
+		padding-inline: ${({fullWidth}) => fullWidth ? '0rem' : '2.5rem'};
 	}
 	@media (min-width: 1440px) {
-		padding-inline: 10.25rem;
+		padding-inline: ${({fullWidth}) => fullWidth ? '0rem' : '10.25rem'};
 	}
 `
 
