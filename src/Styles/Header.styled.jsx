401f8c812bdf0786bf0158bc29e7e1bc6d2d2920
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-19 09:40:12 +1300

    set up the media screens
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Styles/Header.styled.jsx
+++ src/Styles/Header.styled.jsx
@@ -4,6 +4,10 @@ const StyledHeader = styled.header`
 	width: 100%;
 	display: grid;
 	place-items: center;
+	gap: 2rem;
+	@media (min-width: 768px) {
+		gap: 4rem;
+	}
 `
 
 export default StyledHeader
