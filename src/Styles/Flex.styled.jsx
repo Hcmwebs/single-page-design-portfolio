33d1fcaf06c5435ecb13f93096378793b1d6ca31
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-23 09:01:44 +1300

    added a  width 100%
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Styles/Flex.styled.jsx
+++ src/Styles/Flex.styled.jsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components'
 
 const StyledFlex = styled.div`
+	width: 100%;
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
 	gap: 1.5rem;
 
-  &.mainCard3{
-    width: ;
-    flex-direction: column;
-
-  }
+	&.mainCard3 {
+		width: ;
+		flex-direction: column;
+	}
 `
 
 export default StyledFlex
