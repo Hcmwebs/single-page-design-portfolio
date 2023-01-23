6f67d8a5795b2dbbbcc96b6505007f4814f0bdf7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-24 09:12:47 +1300

    created, indexed and rendered MainCard component

--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,44 +1,14 @@
 import React from 'react'
-import { mainCard1, mainCard2, mainCard3,mainCard4 } from '../data/Data'
-import {
-	StyledContainer,
-	StyledMain,
-	StyledMainCard,
-	StyledCard,
-	StyledFlex,
-} from '../Styles'
+import { MainCard } from './index'
 
-import { Card } from './index'
+import { StyledContainer, StyledMain } from '../Styles'
 
 const MainContent = () => {
 	return (
 		<StyledContainer>
 			<StyledMain>
 				<section className='services'>
-					<StyledMainCard>
-						{mainCard1.map((card) => {
-							return <Card key={card.id} {...card} />
-						})}
-					</StyledMainCard>
-
-					<StyledMainCard className='mainCard'>
-						<StyledFlex>
-							{mainCard2.map((card) => {
-								return <Card key={card.id} {...card} />
-							})}
-						</StyledFlex>
-						<StyledCard className='card4'>
-							{mainCard3.map((card) => {
-								return <Card key={card.id} {...card} />
-							})}
-						</StyledCard>
-					</StyledMainCard>
-
-					<StyledMainCard className='mainCard mainCard3'>
-						{mainCard4.map((card) => {
-							return <Card key={card.id} {...card} />
-						})}
-					</StyledMainCard>
+					<MainCard />
 				</section>
 			</StyledMain>
 		</StyledContainer>
