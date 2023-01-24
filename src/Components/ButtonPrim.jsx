bebac1fa0d056df9496d8ae52a67d05872e1df32
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-25 10:02:58 +1300

    renamed the Button component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

---
+++ src/Components/ButtonPrim.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledButton } from '../Styles'
+
+const ButtonPrim = () => {
+	return (
+		<StyledButton  primary as='a' href='#' rel='noopener noreferrer' target='_blank'>
+			Free Consultation
+		</StyledButton>
+	)
+}
+
+export default ButtonPrim
