a3d6222d8a0f8a3641cccacca796f66038a2283e
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-01-28 09:18:41 +1300

    modified the animations of the logo
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { StyledLogo } from '../Styles'
-import { motion } from 'framer-motion'
-import { keyframes } from 'styled-components'
 
 const Logo = () => {
 	return (
 		<>
-			<motion.a
-				href='#'
-				whileHover={{ rotate: 180 }}
-				transition={{ duration: 0.8 }}>
+			<a href='#'>
 				<StyledLogo src='/Assets/logo.svg' alt='logo' />
-			</motion.a>
+			</a>
 		</>
 	)
 }
